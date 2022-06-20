07a435566748f7f6e88686e6952c63560bf58cb9
Antoine Grenier <agrenier@grasset.qc.ca>
2022-06-20 17:17:49 -0400

    première implémentation

--- sdk_laboratoire2/sdk_laboratoire2/Program.cs
+++ sdk_laboratoire2/sdk_laboratoire2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Imporation de la librairie
+using LibrairieConversionNote;
+
+//Création nouvel objet de notre librairie
+Conversion conversionObj = new Conversion();
+
+//On roule l'application non-stop, tant que CTRL+C
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
+
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            var noteRetour = conversionObj.AlphaToNote(note);
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+    //Si l'utilisateur tape alpha
+    else if (input?.ToUpperInvariant() == "ALPHA")
+    {
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            var noteRetour = "Je ne suis pas implémentée"; //Mettre votre appel ici;
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
