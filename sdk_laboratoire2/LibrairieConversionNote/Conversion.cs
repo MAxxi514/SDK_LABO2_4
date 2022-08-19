d5f01b4d22b9b414b5f5083bb1d270baa94b71c6
Anthoni711 <aph.generation@gmail.com>
2022-08-19 17:24:42 -0400

    réaliser la méthode NoteToAlpha(string str)

--- sdk_laboratoire2/LibrairieConversionNote/Conversion.cs
+++ sdk_laboratoire2/LibrairieConversionNote/Conversion.cs
@@ -55,6 +55,36 @@ namespace LibrairieConversionNote
         public string NoteToAlpha(string str)
         {
             string strReturnValue = string.Empty;
+
+            switch (str.ToUpperInvariant())
+            {
+                case "LA":
+                    strReturnValue = "A";
+                    break;
+                case "SI":
+                    strReturnValue = "B";
+                    break;
+                case "DO":
+                    strReturnValue = "C";
+                    break;
+                case "RE":
+                    strReturnValue = "D";
+                    break;
+                case "MI":
+                    strReturnValue = "E";
+                    break;
+                case "FA":
+                    strReturnValue = "F";
+                    break;
+                case "SOL":
+                    strReturnValue = "G";
+                    break;
+                default: //Valeur n'est pas dans la liste du haut
+                    strReturnValue = "L'alpha n'existe pas";
+                    break;
+            }
+
+            //Retourne la valeur
             return strReturnValue;
         }
     }
