697e606c07fae1fac7e4689a6d6117f9e656dcf5
sarahlilianjohnson <sarahlilianjohnson@gmail.com>
2022-08-24 11:01:13 -0400

    Commit avec les changements au code

---
+++ sdk_laboratoire2/LibrairieConversionNote/Conversion.cs
@@ -0,0 +1,92 @@
+﻿//Notre librairie
+namespace LibrairieConversionNote
+{
+    /// <summary>
+    /// Classe de conversion
+    /// </summary>
+    public class Conversion
+    {
+        /// <summary>
+        /// Retourne la note du alphanumerique
+        /// </summary>
+        /// <param name="str">La note alphanumerique</param>
+        /// <returns></returns>
+        public string AlphaToNote(string str)
+        {
+            //variable de retour
+            string strReturnValue = string.Empty;
+            //On mets toutes les valeurs passées en majuscules
+            switch (str.ToUpperInvariant())
+            {
+                case "A":
+                    strReturnValue = "LA";
+                    break;
+                case "B":
+                    strReturnValue = "SI";
+                    break;
+                case "C":
+                    strReturnValue = "DO";
+                    break;
+                case "D":
+                    strReturnValue = "RE";
+                    break;
+                case "E":
+                    strReturnValue = "MI";
+                    break;
+                case "F":
+                    strReturnValue = "FA";
+                    break;
+                case "G":
+                    strReturnValue = "SOL";
+                    break;
+                default: //Valeur n'est pas dans la liste du haut
+                    strReturnValue = "La note n'existe pas";
+                    break;
+            }
+            //Retourne la valeur
+            return strReturnValue;
+        }
+
+        /// <summary>
+        /// Retourne l'alphanumerique de la note
+        /// </summary>
+        /// <param name="str">La note musicale</param>
+        /// <returns></returns>
+
+        public string NoteToAlpha(string str)
+        {
+            //variable de retour
+            string strReturnValue = string.Empty;
+            //On mets toutes les valeurs passées en majuscules
+            switch (str.ToUpperInvariant())
+            {
+                case "LA":
+                    strReturnValue = "A";
+                    break;
+                case "SI":
+                    strReturnValue = "B";
+                    break;
+                case "DO":
+                    strReturnValue = "C";
+                    break;
+                case "RE":
+                    strReturnValue = "D";
+                    break;
+                case "MI":
+                    strReturnValue = "E";
+                    break;
+                case "FA":
+                    strReturnValue = "F";
+                    break;
+                case "SOL":
+                    strReturnValue = "G";
+                    break;
+                default: //Valeur n'est pas dans la liste du haut
+                    strReturnValue = "La note n'existe pas";
+                    break;
+            }
+            //Retourne la valeur
+            return strReturnValue;
+        }
+    }
+}
---
+++ sdk_laboratoire2/sdk_laboratoire2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Imporation de la librairie
+using LibrairieConversionNote;
+
+//Création nouvel objet de notre librairie
+Conversion conversionObj = new Conversion();
+
+//On roule l'application non-stop, tant que CTRL+C
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
+
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            var noteRetour = conversionObj.AlphaToNote(note);
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+    //Si l'utilisateur tape alpha
+    else if (input?.ToUpperInvariant() == "ALPHA")
+    {
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            var noteRetour = conversionObj.NoteToAlpha(note);
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
